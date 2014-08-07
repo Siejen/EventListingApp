1254fb46b8d819b9c52963f7be9c5478b79d1d35
Siejen Yin-Stevenson <siejenyin@gmail.com>
2014-08-06 20:18:29 -0700

    bcrypt, authorization and authentication has been added to app

--- app.js
+++ app.js
@@ -41,6 +41,56 @@ app.use(function(req, res, next){
   next();
 });
 
+passport.use('user-local', new LocalStrategy({
+		usernameField: 'username',
+		passwordField: 'password',
+		passReqToCallback: true
+	},
+	function(req, username, password, done){
+		// Load hash from your password DB.
+		console.log( 'authenticating with user-local:' );
+		console.log( '... username: ' + username );
+		console.log( '... password: ' + password );
+
+		db.user.find({ where: { username: username }})			// 1st username is the key coming from the model file & the 2nd username is value coming from the form
+		.done(function(error,user) {
+			if(error){
+				console.log(error);
+				return done(error, req.flash('loginMessage', 'Oops! Something went wrong.'));
+			}
+			if(user === null) {
+				console.log("... username not found in database");
+				return done(null, false, req.flash('loginMessage', 'Username does not exist.'));
+			}
+			// compareSync is a method in bcrypt
+			if(!bcrypt.compareSync(password, user.passwordHash)) {		// "password" comes from the form in clear text and "user.passwordHash" is the hashed password stored in the DB.  Bcrypt hashes the clear text password and compares it against the hashed one store in database.
+				console.log("--------------------------------");
+				console.log(password);
+				console.log(user.passwordHash);
+				console.log("--------------------------------");
+				//'user.passwordHash' is from the database & 'password' is submitted through the client browser
+				return done(null, false, req.flash('loginMessage', 'Invalid Password'));
+			}
+			// no errors and password match!  This sets up the session.
+			done(null, user);		// null ~ no error
+		});
+}));
+
+passport.serializeUser(function(user, done) {
+	console.log("SERIALIZED JUST RAN!");
+	done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+	console.log("DESERIALIZATION JUST RAN!");
+	// In line 87, the 1st id is the key (and the name of the column in the DB)
+	// & 2nd id is the id (sitting in the server) paired with the cookie			
+	db.user.find( {where: { id: id }} ).
+	done(function(err, user) {
+		done(err,user);
+	});
+});
+
 app.get('/', function(req, res){
 	// find all the posts in the database
 	// in the future, when that is complete, invoke the callback function
@@ -63,19 +113,21 @@ app.get('/signup', function(req, res){
 
 app.post('/signup', function(req, res){
 	console.log( req.body );
+	var salt = bcrypt.genSaltSync(10);
+	var hash = bcrypt.hashSync(req.body.password, salt);	// salting the password, hashing the "password" (user inputted into the form during signup process) and storing it in the variable hash
 	var postInfo = {
 		// keys: model.js
 		// values: from the form (req.body.name-of-field)
 		organizationName: req.body.organizationName,
 		username: req.body.username,
-		passwordHash: req.body.passwordHash,
+		passwordHash: hash,									// passwordHash will be the result of bcrypt.hashSync
 		organizationURL: req.body.organizationURL,
 		firstname: req.body.firstname,
 		lastname: req.body.lastname,
 		email: req.body.email
 	};
-	db.user.create(req.body);
-	res.redirect('logon');
+	db.user.create(postInfo);
+	res.redirect('/logon');
 });
 
 app.get('/logon', function(req, res){
@@ -83,14 +135,16 @@ app.get('/logon', function(req, res){
 	res.render('user-logon');
 });
 
-app.post('/logon', function(req, res){
-	res.redirect('profile');
-});
+app.post('/logon', passport.authenticate('user-local', {
+	successRedirect: '/profile',
+    failureRedirect: '/logon',
+    failureFlash: true 
+}));
 
 app.get('/profile', function(req, res){
-	db.user.find({ where: {id: 1}}).complete(function(err, user) {
+	db.user.find({ where: {id: req.user.id}}).complete(function(err, user) {
 		db.post.findAll().complete(function(err, posts) {
-			var email = "siejenyin@gmail.com";
+			var email = req.user.email;
 			var trimmedEmail = email.trim();
 			var lctEmail = trimmedEmail.toLowerCase();
 			var hash = md5(lctEmail);
@@ -125,43 +179,10 @@ app.post('/addevent', function(req, res){
 	});
 });
 
-// app.get('/student/submitfist/:id', function(req,res){
-// 	if(req.isAuthenticated() && req.user.dataValues.isStudent ){
-// 		db.student.find({ where: {id: req.user.id }}).complete(function(err, student) {
-// 			var info = {
-// 				student:  student,
-// 				surveyId: req.params.id
-// 			};
-// 			res.render('student/student-response', info);
-// 		});
-// 	}
-// 	else {
-// 		res.redirect('/');	
-// 	}	
-// });
-
-// app.post('/student/submitfist', function(req,res){
-// 	if(req.isAuthenticated() && req.user.dataValues.isStudent ){	
-// 		console.log(req.body);
-// 		var fistInfo = {
-// 			ranking: req.body.ranking,
-// 			comment: req.body.comment,
-// 			surveyId: req.body.surveyId,
-// 			studentId: req.user.id
-// 		};
-// 		db.fist.create( fistInfo )
-// 		.success(function(fist){
-// 			req.user.addFist(fist);
-// 			res.redirect('view');
-// 		});
-
-// 	}
-// 	else {
-// 		res.redirect('/');	
-// 	}	
-// });
-
-
+app.get('*', function(req,res){
+  res.status(404);
+  res.render('404');
+});
 
 app.listen(3000, function(){
   console.log("LISTENING ON PORT 3000");
